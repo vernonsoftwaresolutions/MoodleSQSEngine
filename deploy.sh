5ba4784804ec054a1574c3fe5234f2ea89c356b5
andrewlarsen <andrew.larsen@concordusa.com>
2017-08-25 00:02:33 -0500

    last one

--- deploy.sh
+++ deploy.sh
@@ -35,7 +35,7 @@ aws cloudformation package --template-file \
 ##
 aws cloudformation deploy --template-file \
     formation_assets_output.yaml --capabilities CAPABILITY_IAM \
-    --stack-name "${API_NAME}" StageName="${ENV}"
+    --stack-name ${API_NAME} StageName=${ENV}
 
 ##
 # get the api gateway ref
