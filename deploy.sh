3a9750445ce119a76ea2b58dab6fd70643df5afd
andrewlarsen <andrew.larsen@concordusa.com>
2017-08-24 23:51:08 -0500

    add params

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ aws cloudformation package --template-file \
 ##
 aws cloudformation deploy --template-file \
     formation_recordset_output.yaml --capabilities CAPABILITY_IAM \
-    --stack-name apidnsname
+    --stack-name apidnsname --parameter-overrides HostedZoneName="${DNSNAME}" RecordSet="${RecordSet}"
 ##
 # Package API Gateway Assets
 ##
@@ -35,7 +35,7 @@ aws cloudformation package --template-file \
 ##
 aws cloudformation deploy --template-file \
     formation_assets_output.yaml --capabilities CAPABILITY_IAM \
-    --stack-name ${API_NAME} --parameter-overrides HostedZoneName="${DNSNAME}" RecordSet="${RecordSet}"
+    --stack-name "${API_NAME}" StageName="${ENV}"
 
 ##
 # get the api gateway ref
