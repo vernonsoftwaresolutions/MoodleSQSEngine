179060400530c08053968a7de223f2018624490f
andrewlarsen <andrew.larsen@concordusa.com>
2017-09-09 11:58:07 -0500

    remove check for size not equal to 1

--- src/main/java/com.moodle.tenant/cloudformation/client/CloudformationClientImpl.java
+++ src/main/java/com.moodle.tenant/cloudformation/client/CloudformationClientImpl.java
@@ -30,7 +30,7 @@ public class CloudformationClientImpl implements CloudformationClient{
 
         DescribeStacksResult result = this.cloudFormation.describeStacks(request);
         logger.info("Returned result {} ", result);
-        if(result.getStacks().isEmpty() || result.getStacks().size() > 1){
+        if(result.getStacks().isEmpty()){
             logger.info("Result size is invalid");
             return Optional.empty();
         }
