6ba60ef082f22765f4bd60d7252d920f1604f99a
andrewlarsen <andrew.larsen@concordusa.com>
2017-09-09 12:37:32 -0500

    add || true instead of exit 0

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,21 @@ S3_BUCKET=$1
 DEPLOY_BUCKET=$2
 ARTIFACT=$3
 ENV=$4
+VPC=$5
 # upload artifact to s3
 aws s3 cp dir/"${ARTIFACT}" s3://"${DEPLOY_BUCKET}"/"${ARTIFACT}"
 ##Create application
 aws cloudformation package --template-file elasticbeanstalk_sqs_application.yml \
     --output-template-file elasticbeanstalk_sqs_application_output.yml --s3-bucket \
     "${S3_BUCKET}"
-# note the || exit 0, this will not fail the build if the command fails.  I know...but unfortunately the
+# note the || true, this will not fail the build if the command fails.  I know...but unfortunately the
 # resource in this template is not able to be deployed idempotently =/ (not a word)
-aws cloudformation deploy --template-file elasticbeanstalk_sqs_application.yml --capabilities CAPABILITY_IAM --stack-name eb-sqs-app || exit 0
+aws cloudformation deploy --template-file elasticbeanstalk_sqs_application.yml --capabilities CAPABILITY_IAM --stack-name eb-sqs-app || true
 ## create deployment
 aws cloudformation package --template-file elasticbeanstalk_sqs.yml --output-template-file \
     elasticbeanstalk_sqs_output.yml --s3-bucket "${S3_BUCKET}"
 
 aws cloudformation deploy --template-file elasticbeanstalk_sqs_output.yml --capabilities CAPABILITY_IAM \
-    --stack-name eb-sqs --parameter-overrides VpcId=vpc-c7aa77be DeploymentBucket=elasticbeanstalk-moodle-deployables \
+    --stack-name eb-sqs --parameter-overrides VpcId="${VPC}" DeploymentBucket="${S3_BUCKET}" \
     DeploymentVersion="${ARTIFACT}" Environment="${ENV}"
 
